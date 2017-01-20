12a4c9282f4e8865fa082d9d06f834e2eba0da7a
kiiiiNNNNNNNNNyyyy <ad.sam007@gmail.com>
2017-01-20 16:00:48 -0500

    Started Building guess the celebrity app

---
+++ GuesstheCelebrity/app/src/main/java/com/example/user/guessthecelebrity/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.user.guessthecelebrity;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try{
+
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while(data != -1){
+
+                    char current = (char) data;
+                    result +=  current;
+                    data = reader.read();
+
+                }
+
+                return result;
+
+            }catch(Exception e){
+
+                e.printStackTrace();
+
+            }
+
+            return null;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        String result = null;
+
+        try {
+
+            result = task.execute("http://www.posh24.com/celebrities").get();
+            Log.i("Contents of URL: ", result);
+
+        }catch (InterruptedException e) {
+
+            e.printStackTrace();
+
+        }catch (ExecutionException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+}
