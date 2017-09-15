73da1a05dfcf6e61e721ac73034a0ef9b2448a0d
Arjun  Dass <ad.sam007@gmail.com>
2017-09-15 10:42:30 -0500

    Implemented additional action bar items

--- Basic Usage of elements/SharedPreferencesdemo/app/src/main/java/com/example/kinny/sharedpreferencesdemo/MainActivity.java
+++ Basic Usage of elements/SharedPreferencesdemo/app/src/main/java/com/example/kinny/sharedpreferencesdemo/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,24 @@ public class MainActivity extends AppCompatActivity {
         String username = sharedPreferences.getString("username", "");
         Log.i("username", username);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if(id == R.id.add){
+
+            Toast.makeText(getApplicationContext(), "Action button tapped", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
